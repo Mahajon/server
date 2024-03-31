dffc8bd59b657979e9af33f6b0f044fc7f7a142d
Fardeen Ehsan <fardeen.es7@gmail.com>
2024-03-31 07:29:13 +0600

    added shop admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import Shop
 # Register your models here.
+
+
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'created_at', 'updated_at')
+    search_fields = ('name', 'owner')
+    list_filter = ('created_at', 'updated_at')
+
+    def save_model(self, request, obj, form, change):
+        # Logic to handle changes made by admins
+        if 'is_active' in form.changed_data and not request.user.is_superuser:
+            # If is_active field has been changed by a non-superuser, revert it
+            obj.is_active = not obj.is_active  # Toggle the value
+        super().save_model(request, obj, form, change)
+
+
+admin.site.register(Shop, ShopAdmin)
