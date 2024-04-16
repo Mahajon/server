8baafaf678f4f247ff49c2cc3283e01a94e12687
Fardeen Ehsan <fardeen.es7@gmail.com>
2024-04-16 21:57:14 +0600

    updated settings

--- mahajon/settings.py
+++ mahajon/settings.py
@@ -145,9 +145,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 
