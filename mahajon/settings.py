617e2441dd0a86754e0aca36b3766afe00b28c17
Fardeen Ehsan <fardeen.es7@gmail.com>
2024-04-24 11:52:31 +0600

    moved staticfiles dirs to debug only

--- mahajon/settings.py
+++ mahajon/settings.py
@@ -183,10 +183,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
 
 
 
