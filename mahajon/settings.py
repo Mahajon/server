05102a43c63fc72c49de247908c75df0ddd37192
Fardeen Ehsan <fardeen.es7@gmail.com>
2024-04-24 16:25:01 +0600

    moved staticfiles dirs to debug only

--- mahajon/settings.py
+++ mahajon/settings.py
@@ -153,9 +153,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
 
 
 
